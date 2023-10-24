d54d5c0a6a70a7dce4969469b0c74d21e2b8c913
Dins <@DinS1377>
2023-10-24 18:03:50 +0300

    update

--- PZ-3/task_1/main.py
+++ PZ-3/task_1/main.py
@@ -12,25 +12,16 @@ while True:
             continue
 
 
-# проверка на положительность и отрицательность каждого числа
 if a >= 0:
-    a = True
-else:
-    a = False
-
-if b >= 0:
-    b = True
-else:
-    b = False
-
-if c >= 0:
-    c = True
-else:
-    c = False
-
-
-if (a == True and b == False and c == False or a == False and b == True and c == False or a == False and b == False
-        and c == True):
     print("одно из чисел положительное")
+
+elif b >= 0:
+    print("одно из чисел положительное")
+
+elif c >= 0:
+    print("одно из чисел положительное")
+
 else:
     print("высказывание ложно")
+
+
