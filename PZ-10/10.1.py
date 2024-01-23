2392645b46b68d09f47f3faf9cba7f9475b3cd23
Dins <@DinS1377>
2024-01-23 14:32:26 +0300

    update

--- PZ-10/10.1.py
+++ PZ-10/10.1.py
@@ -5,14 +5,14 @@ reyna_tour = {'england', 'japan', 'canada', 'sar'}
 rainbow = {'usa', 'spain', 'scotland', 'australia', 'italy', 'canada'}
 
 a = {'italy', 'canada'}
-penis = [voyage, reyna_tour, rainbow]
-penis_2_0 = ['voyage', 'reyna_tour', 'rainbow']
+tours = [voyage, reyna_tour, rainbow]
+tours_names= ['voyage', 'reyna_tour', 'rainbow']
 
 # в каких турагенствах туры в италию и канаду
-for i in penis:
+for i in tours:
     b = i & a
     if b == a:
-        print(f'В туре {penis_2_0[c]}')
+        print(f'В туре {tours_names[c]}')
     c = +1
 
 # добавление страны в турагенство
